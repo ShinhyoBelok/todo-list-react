274f3772d3fe464b246bd1cf32161ccc17b94335
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-16 16:55:12 -0400

    Add completed style for the listItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,18 +3,29 @@ import styles from "./TodoItem.module.css"
 
 export default class TodoItem extends Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    const { completed, id, title } = this.props.todo
+
     return (
       <li className={styles.item}>
         <input
           type="checkbox"
           className={styles.checkbox}
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
         <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
           Delete
         </button>
-        {this.props.todo.title}
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     )
   }
