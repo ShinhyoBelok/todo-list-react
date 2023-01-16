adaaa968efea6d3a78ccee7c6f6529d5b2dc7b10
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-16 16:35:30 -0400

    Add general css to the todo-list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import TodoContainer from './components/TodoContainer';
+import "./App.css"
 
 function App() {
   return (
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import styles from "./TodoItem.module.css"
 
 export default class TodoItem extends Component {
   render() {
     return (
-      <li>
-        <input 
-          type="checkbox" 
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-        /> 
+        />
         <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
           Delete
         </button>
