19a9eae05c254ecf7917952fa0a474f50486f606
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-16 16:17:40 -0400

    Add list checkbox functionalities and input field

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,8 +7,11 @@ export default class TodoItem extends Component {
         <input 
           type="checkbox" 
           checked={this.props.todo.completed}
-          onChange={() => console.log("clicked")}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         /> 
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
         {this.props.todo.title}
       </li>
     )
